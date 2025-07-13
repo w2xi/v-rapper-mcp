dc5eeca35abbfbef26cfbb0f5d630afce51e49aa
w2xi <43wangxi@gmail.com>
2025-07-13 22:15:27 +0800

    chore: remove dist

--- dist/cli.js
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env node
-import {McpServer}from'@modelcontextprotocol/sdk/server/mcp.js';import {StdioServerTransport}from'@modelcontextprotocol/sdk/server/stdio.js';function n(){let e=new McpServer({name:"v-rapper-mcp",version:"0.0.1"},{capabilities:{tools:{}}});e.tool("v-rapper",`Get Evan You's v-rapper video URL
-Applicable scenarios:
-1. User asks about v-rapper
-2. User asks about VueConf 2025 Evan You rap video
-`,{},async()=>({content:[{type:"text",text:"https://www.bilibili.com/video/BV1U9MXzvEHR/?vd_source=d36c0f862962bd8f8a7256fb2ddb5bbf"}]}));let r=new StdioServerTransport;e.connect(r);}n();
